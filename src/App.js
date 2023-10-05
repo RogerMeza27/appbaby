da1e3569750f8b32535f28136ca83d308ec38c5c
RogerMeza27 <142762440+RogerMeza27@users.noreply.github.com>
2023-10-05 01:58:23 -0500

    Add Products y firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Products } from "./components/Products";
+import { Projects } from "./components/Projects";
 import { Footer } from "./components/Footer";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <Products />
+      <Projects />
       <Footer />
     </div>
   );
